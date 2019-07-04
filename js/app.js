5fcb117775b20f952ce859308ef856f5c392c419
osnat <osnatlev32@walla.co.il>
2019-07-04 21:08:02 +0300

    init commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//show cart
+(function() {
+	const cartInfo = document.getElementById('cart-info');
+	const cart = document.getElementById('cart');
+	cartInfo.addEventListener('click', function() {
+		cart.classList.toggle('show-cart');
+	});
+})();
